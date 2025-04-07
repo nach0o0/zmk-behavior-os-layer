d6fd1d78a1030fe9b52c0204876028481858e003
nach0o0 <109413053+nach0o0@users.noreply.github.com>
2025-04-07 11:52:23 +0200

    fixed driver functions

--- src/behaviors/behavior_os_layer.c
+++ src/behaviors/behavior_os_layer.c
@@ -11,7 +11,6 @@
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 static char current_host_os[30] = "windows";
-static char windows_host_os[30] = "windows";
 
 struct behavior_os_layer_config {
     int linux_layer;
@@ -31,7 +30,7 @@ static int behavior_os_layer_keymap_binding_pressed(struct zmk_behavior_binding 
 
     if (strcmp(current_host_os, "linux") == 0) {
         layer_to_activate = cfg->linux_layer;
-    } else if (strcmp(current_host_os, windows_host_os) == 0) {
+    } else if (strcmp(current_host_os, "windows") == 0) {
         layer_to_activate = cfg->windows_layer;
     } else if (strcmp(current_host_os, "macos") == 0) {
         layer_to_activate = cfg->macos_layer;
@@ -43,14 +42,31 @@ static int behavior_os_layer_keymap_binding_pressed(struct zmk_behavior_binding 
         layer_to_activate = cfg->default_layer;
     }
 
-    zmk_keymap_layer_to(layer_to_activate);
-    return ZMK_BEHAVIOR_OPAQUE;
+    return zmk_keymap_layer_activate(layer_to_activate);
 }
 
 static int behavior_os_layer_keymap_binding_released(struct zmk_behavior_binding *binding,
                                                      struct zmk_behavior_binding_event event) {
     
-    return ZMK_BEHAVIOR_OPAQUE;
+    const struct behavior_os_layer_config *cfg = zmk_behavior_get_binding(binding->behavior_dev)->config;
+
+    int layer_to_deactivate = cfg->default_layer;
+
+    if (strcmp(current_host_os, "linux") == 0) {
+        layer_to_deactivate = cfg->linux_layer;
+    } else if (strcmp(current_host_os, "windows") == 0) {
+        layer_to_deactivate = cfg->windows_layer;
+    } else if (strcmp(current_host_os, "macos") == 0) {
+        layer_to_deactivate = cfg->macos_layer;
+    } else if (strcmp(current_host_os, "android") == 0) {
+        layer_to_deactivate = cfg->android_layer;
+    } else if (strcmp(current_host_os, "ios") == 0) {
+        layer_to_deactivate = cfg->ios_layer;
+    } else {
+        layer_to_deactivate = cfg->default_layer;
+    }
+
+    return zmk_keymap_layer_deactivate(layer_to_deactivate);
 }
 
 static const struct behavior_driver_api behavior_os_layer_driver_api = {
